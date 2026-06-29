757af0f59ccaf81c2ac3b1490fc9166a0fcf80fd
vitaliikuzmenko3 <vkuzmenkov60@gmail.com>
2026-06-29 17:22:54 +0200

    Crawler - update
    Updating the function hierarchy.
    Translated into English

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 import random
 import re
@@ -7,115 +8,102 @@ from bs4 import BeautifulSoup,SoupStrainer
 
 class Crawler:
     def __init__(self):
-        self._data = []
         self.MAIN_URL = "https://reality.bazos.sk/"
         self.HEADERS=  {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
         "Referer": "https://www.google.com/"
         }
 
-        self.popis_filter_inzerat = SoupStrainer("div", class_="maincontent")
+        self.descript_filter_publication = SoupStrainer("div", class_="maincontent")
         self.filter_main = SoupStrainer("div", class_=["maincontent", "strankovani"])
         self.exceptions = {"Dohodou", "Zadarmo"}
-
-        self.already_exist = set()
-
-
-    def start_process(self):
-        links = [
+        self.links = [
         ["https://reality.bazos.sk/prenajmu/byt/","https://reality.bazos.sk/prenajmu/byt/?hledat=&rubriky=reality&hlokalita=81101&humkreis=10&cenaod=&cenado=&order=&crp=&kitx=ano"]
         #["https://reality.bazos.sk/prenajmu/dom/","https://reality.bazos.sk/prenajmu/dom/?hledat=&rubriky=reality&hlokalita=81101&humkreis=10&cenaod=&cenado=&Submit=H%C4%BEada%C5%A5&order=&crp=&kitx=ano"]
         #["https://reality.bazos.sk/prenajmu/podnajom/","https://reality.bazos.sk/prenajmu/podnajom/?hledat=&rubriky=reality&hlokalita=81101&humkreis=10&cenaod=&cenado=&order=&crp=&kitx=ano"]
         ]
-        for link in links:
-            self.crawl_bazos(link[0],link[1])
-    def add_inzerat(self, inzerat_dict):
-        self._data.append(inzerat_dict)
-    def get_data(self):
-        return self._data
-    def crawl_bazos(self,link_first_main,link_with_sorted):
-        previous_page = None
-        number = 0
 
+
+    def start_processing(self):
+        data = []
+        for link in self.links:
+            data.extend(self.crawl_bazos(link[0], link[1]))
+        return data
+    def crawl_bazos(self,link_first_main,link_with_sorted):
         session = requests.Session()
         session.headers.update(self.HEADERS)
 
         session.get(self.MAIN_URL)
-        time.sleep(random.randint(1,3))
+        time.sleep(random.randint(1, 3))
         session.get(link_first_main)
-        time.sleep(random.randint(1,3))
-        curr_page = link_with_sorted
+        time.sleep(random.randint(1, 3))
 
-        response = session.get(curr_page)
+        first_response = session.get(link_with_sorted)
         time.sleep(random.uniform(1, 2))
 
-        pocet_iteracie = self.pocet_iteracie_getter(response)
+        total_pages = self.number_iteration_getter(first_response)
+        return self.process_main(session, link_with_sorted, first_response, total_pages)
 
-
-        while pocet_iteracie > number:
-            if previous_page:
-                session.headers.update({"Referer": previous_page})
+    def process_main(self,session,start_url,first_response,total_pages):
+        data = []
+        curr_page = start_url
+        response = first_response
+        previous_page = None
+        for pag_index in range(total_pages):
+            if pag_index > 0:
+                if previous_page:
+                    session.headers.update({"Referer": previous_page})
                 response = session.get(curr_page)
                 time.sleep(random.uniform(1, 2))
-
-            soup = BeautifulSoup(response.text,"lxml",parse_only = self.filter_main)  #,parse_only = self.filter_main
-
-            inzeraty = self.all_advertisement_getter(soup)
-            for inzerat in inzeraty:
-
-                url_inzerat = self.link_getter(inzerat)
-
-                if url_inzerat in self.already_exist:
-                    continue
-
-                cena = self.cena_getter(inzerat)
-
-                if cena in self.exceptions:
-                    continue
-
-                lokalita = self.lokalita_getter(inzerat)
-
-                session.headers.update({"Referer": curr_page})
-
-
-                soup_inzerat = self.move_to_inzerat(session,url_inzerat)
-
-                title = self.title_inzerat(soup_inzerat)
-
-                date = self.date_of_post(soup_inzerat)
-
-                description = self.description_inzerat(soup_inzerat)
-
-                cena_text = f"Cena tohto bytu je {cena}.\n" if cena != "V texte" else ""
-
-                final_text = (
-                    f"Title: {title}\n"
-                    f"{description}\n"
-                    f"{cena_text}"
-                    f"Lokalita je {lokalita}."
-                )
-
-                current_time = time.localtime()
-                formatted_data_curr_time = time.strftime("%d-%m-%Y", current_time)
-
-                data_dict = self.construct_dict_input(source="Bazos",link=url_inzerat,
-                             description=final_text,state="Not proccessed",
-                             data_post=date,data_crawler=formatted_data_curr_time)
-
-                self.add_inzerat(data_dict)
-                self.already_exist.add(url_inzerat)
-                print(data_dict)
-
+            soup = BeautifulSoup(response.text, "lxml", parse_only=self.filter_main)
+            data.extend(self.process_page(session, soup, curr_page))
             previous_page = curr_page
-
             next_page_url = self.next_main_page(soup)
             if not next_page_url:
                 break
-            print(number)
-            number +=1
             curr_page = next_page_url
+        return data
+    def process_page(self,session,soup,page_url):
+        data = []
+        accommodations = self.all_advertisement_getter(soup)
+        for accommodation in accommodations:
+            publication = self.process_publication(session, accommodation, page_url)
+            if publication:
+                data.append(publication)
+        return data
 
-    def pocet_iteracie_getter(self, responce):
+    def process_publication(self,session,accommodation,page_url):
+        url_publication = self.link_getter(accommodation)
+
+        price = self.price_getter(accommodation)
+        if price in self.exceptions:
+            return
+        location = self.location_getter(accommodation)
+        session.headers.update({"Referer": page_url})
+
+        soup_aparment = self.move_to_inzerat(session,url_publication)
+
+        title = self.title_publication(soup_aparment)
+        date = self.date_of_post(soup_aparment)
+        description = self.description_publication(soup_aparment)
+        price_text = f"Cena tohto bytu je {price}.\n" if price != "V texte" else ""
+
+        final_text = (
+            f"Title: {title}\n"
+            f"{description}\n"
+            f"{price_text}"
+            f"Lokalita je {location}."
+        )
+        return self.construct_dict_input(
+            source="Bazos",
+            link=url_publication,
+            description=final_text,
+            state="Not proccessed",
+            data_post=date,
+            data_crawler=datetime.now().strftime("%Y-%m-%d")
+        )
+
+    def number_iteration_getter(self, responce):
         #main_hub_once
         soup = BeautifulSoup(responce.text, "lxml", parse_only=self.filter_main)
         stats_tag = soup.find('div', class_='inzeratynadpis')
@@ -127,45 +115,48 @@ class Crawler:
         #main_hub
         return soup.find_all('div', class_="inzeraty inzeratyflex")
 
-    def link_getter(self,inzerat):
-        #main_hub_inzerat
-        link_inzer_tag = inzerat.find('div', class_='inzeratynadpis')
-        a_tag = link_inzer_tag.find('a', href=True)
+    def link_getter(self,publication):
+        #main_hub_publication
+        link_publication_tag = publication.find('div', class_='inzeratynadpis')
+        a_tag = link_publication_tag.find('a', href=True)
         raw_href = a_tag['href']
         return "https://reality.bazos.sk" + raw_href
 
-    def cena_getter(self,inzerat):
-        #main_hub_inzerat
-        cena_tag = inzerat.find('div', class_='inzeratycena')
-        return cena_tag.text.strip()
+    def price_getter(self,publication):
+        #main_hub_publication
+        price_tag = publication.find('div', class_='inzeratycena')
+        return price_tag.text.strip()
 
-    def lokalita_getter(self,inzerat):
-        #main_hub_inzerat
-        lokalita_tag = inzerat.find('div', class_='inzeratylok')
-        lokalita = lokalita_tag.get_text(separator=" ").strip()
-        return " ".join(lokalita.split())
+    def location_getter(self,publication):
+        #main_hub_publication
+        location_tag = publication.find('div', class_='inzeratylok')
+        location = location_tag.get_text(separator=" ").strip()
+        return " ".join(location.split())
 
-    def move_to_inzerat(self,session,url_inzerat):
-        #from main to inzerat
-        response_inzerat = session.get(url_inzerat)
+    def move_to_inzerat(self,session,url_publication):
+        #from main to publication
+        response_publication = session.get(url_publication)
         time.sleep(random.uniform(1, 2))
-        soup_inzerat = BeautifulSoup(response_inzerat.text, "lxml",
-                                     parse_only=self.popis_filter_inzerat)
-        return soup_inzerat
+        soup_publication = BeautifulSoup(response_publication.text, "lxml",
+                                     parse_only=self.descript_filter_publication)
+        return soup_publication
 
-    def title_inzerat(self,soup_inzerat):
-        #title from inzerat
-        h1_tag = soup_inzerat.find('h1', class_='nadpisdetail')
+    def title_publication(self,soup_publication):
+        #title from publication
+        h1_tag = soup_publication.find('h1', class_='nadpisdetail')
         return h1_tag.text.strip()
 
-    def date_of_post(self,soup_inzerat):
-        #date of post from inzerat
-        date_tag = soup_inzerat.find('span', class_='velikost10')
+    def date_of_post(self,soup_publication):
+        #date of post from publication
+        date_tag = soup_publication.find('span', class_='velikost10')
         date = re.search(r'\[(.+?)\]', date_tag.text)
-        return  date.group(1).replace(" ", "").strip().replace("-",".")
+        date = date.group(1).replace(" ", "").strip().replace("-",".")
+        parts = date.split(".")
+        day,month,year = int(parts[0]),int(parts[1]),int(parts[2])
+        return  datetime(year,month,day).strftime("%Y-%m-%d")
 
-    def description_inzerat(self,soup_inzerat):
-        paragraphs = soup_inzerat.select('div.popisdetail')
+    def description_publication(self,soup_publication):
+        paragraphs = soup_publication.select('div.popisdetail')
         return '\n'.join([paragraph.text.strip() for paragraph in paragraphs])
 
     def construct_dict_input(self,source,link,
@@ -192,9 +183,8 @@ class Crawler:
         return "https://reality.bazos.sk" + next_div['href']
 
 
-#if __name__ == '__main__':
-    #s = Crawler()
-    #s.start_process()
-    #k = s.get_data()
-    #for s in k:
-        #print(s)
+#if __name__ == "__main__":
+    #crawler = Crawler()
+    #data = crawler.start_processing()
+    #for item in data:
+        #print(item)
